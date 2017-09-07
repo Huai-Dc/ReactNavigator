1a80955a6581e793a66f34106eb15d3faefdacd3
qinghuai.huang <837084459@qq.com>
2017-09-07 13:56:07 +0800

    添加 轮播

--- src/components/BarcodeScanner.js
+++ src/components/BarcodeScanner.js
@@ -2,22 +2,41 @@ import React, { Component } from 'react';
 import {
     Text, View, StyleSheet,
 } from 'react-native';
+import BarcodeScanner from 'react-native-barcodescanner';
 
-class BarcodeScanner extends Component{
-    constructor(props){
-        super(props);
-
-        this.state = {
-            barcode:'',
-            cameraType: 'back',
-            text: 'Scan Barcode',
-            torchMode: 'off',
-            type: '',
-        }
-    }
-    render() {
-        return (
-            <View></View>
-        );
-    }
-}
+// class BarcodeScannerBar extends Component{
+//     constructor(props){
+//         super(props);
+//
+//         this.state = {
+//             barcode:'',
+//             cameraType: 'back',
+//             text: 'Scan Barcode',
+//             torchMode: 'off',
+//             type: '',
+//         }
+//     }
+//     barcodeReceived(e){
+//         if(e.data !== this.state.barcode || e.type !== this.state.type)
+//             this.setState({
+//                 barcode: e.data,
+//                 text: '${e.data} (${e.type})',
+//                 type: e.type,
+//             })
+//     }
+//     render() {
+//         return (
+//             <View>
+//                 <BarcodeScanner
+//                     onBarCodeRead={this}
+//                     style={{flex:1}}
+//                     torchMode={this.state.torchMode}
+//                     cameraType={this.state.cameraType}
+//                 />
+//                 <View>
+//                     <Text></Text>
+//                 </View>
+//             </View>
+//         );
+//     }
+// }
